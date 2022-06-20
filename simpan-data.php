33b8076f18b872af939b4414a20767691c2c384d
AhyatS <saputraahyat@gmail.com>
2022-06-21 01:04:34 +0700

    quiz

--- simpan-data.php
+++ simpan-data.php
@@ -13,29 +13,195 @@
 <h2> Isilah Soal Berikut ini</h2>
 <table>
 <form method="post" action="">
-	<tr><td>Nama : </td><td><input type="text" name="nama" style="width:300px"></td></tr>
-	<tr><td>Kelas: </td><td><textarea name="kelas" style="width:300px"></textarea></td></tr>
+	<tr><td>Nama : <input type="text" name="nama" style="width:300px"></td></tr>
+	<tr><td>Kelas: <input name="kelas" style="width:300px"></textarea></td></tr>
 	<tr><td><ol start="1" type="a">
 Pilihan Ganda<br><br>
-isi lah asdhaskdhasd<br><i>
-<input type="radio" name="NO1" value="a">a. Text.<p>
-<input type="radio" name="NO1" value="b">b. LINUX<p>
-<input type="radio" name="NO1" value="c">c. Machintos<p>
-<input type="radio" name="NO1" value="d">d. Password<p>
-<input type="radio" name="NO1" value="e">e. UNIX<p>
+1.Di bawah ini merupakan langkah-langkah dalam menganalisis informasi baik fakta dan opini 
+dalam sebuah artikel, kecuali … <br><i>
+<input type="radio" name="NO1" value="a">a. Analisis informasi <p>
+<input type="radio" name="NO1" value="b">b. Pilih kalimat dari artikel <p>
+<input type="radio" name="NO1" value="c">c. Perhatikan ide pokoknya<p>
+<input type="radio" name="NO1" value="d">d. Baca artikel dengan saksama<p>
+<input type="radio" name="NO1" value="e">e. Cermati kalimat yang diambil <p>
 </i> 
 </ol></tr></td><tr><td><ol start="1" type="a">
-blablablalbla<br><i>
-<input type="radio" name="no2" value="a">a. Text.<p>
-<input type="radio" name="no2" value="b">b. LINUX<p>
-<input type="radio" name="no2" value="c">c. Machintos<p>
-<input type="radio" name="no2" value="d">d. Password<p>
-<input type="radio" name="no2" value="e">e. UNIX<p>
+2. <b>Perhatikan pernyataan-pernyataan berikut!</b><br> 
+(1)sesuai kenyataan isi dan kebenarannya<br>
+(2)bergantung pada kepentingan tertentu <br>
+(3)benar atau salah bergantung data pendukung <br>
+(4)cenderung deskriptif pengungkapannya <br>
+(5)cenderung persuasif pengungkapan <br>
+(6)penalarannya cenderung induktif <br>
+(7)penalarannya cenderung deduktif. <br>
+Berdasarkan pernyataan di atas yang termasuk ciri-ciri fakta adalah ….<br><i>
+<input type="radio" name="no2" value="a">a. (1), (2), (3) <p>
+<input type="radio" name="no2" value="b">b. (1), (4), (6) <p>
+<input type="radio" name="no2" value="c">c. (2), (4), (5) <p>
+<input type="radio" name="no2" value="d">d. (2), (3), (6) <p>
+<input type="radio" name="no2" value="e">e. (3), (4), (5) <p>
 </i> 
+</ol></tr></td><tr><td>
+<ol start="1" type="a">
+3. <b>Bacalah paragraf berikut dengan saksama! </b><br>
+(1) Udara di Bogor terasa dingin. <br>
+(2) Kali ini dinginnya melebihi hari-hari sebelumnya. <br>
+(3) Dinginnya suhu udara di Bogor mencapai 24ºC. <br>
+(4) Data tingkat suhu udara ini, terdapat di papan informasi pengukur suhu di jalan-jalan besar di kota Bogor.<br> 
+Dua kalimat pendapat pada teks tersebut ditandai dengan nomor ....<br><i>
+<input type="radio" name="no3" value="a">a. (1) dan (2) <p>
+<input type="radio" name="no3" value="b">b. (1) dan (3) <p>
+<input type="radio" name="no3" value="c">c. (1) dan (4) <p>
+<input type="radio" name="no3" value="d">d. (2) dan (3) <p>
+<input type="radio" name="no3" value="e">e. (2) dan (4) <p>
+</i> 
+</ol></tr></td><tr><td>
+<ol start="1" type="a">
+4. <b>Cermati kutipan teks berikut untuk menjawab soal nomor 5 dan 6! </b><br>
+Olahraga sangat membantu pertahanan tubuh agar terhindar dari berbagai penyakit. Rajin 
+berolahraga, antara lain, membuat kita dapat tidur nyenyak. Olahraga juga membuat kita 
+menjadi orang yang selalu ceria sehingga terhindar dari berbagai macam penyakit, termasuk 
+kanker. <br>
+Pendapat yang dapat mewakili pernyataan itu adalah …. <br><i>
+<input type="radio" name="no2" value="a">a. Rajin berolahraga sudah pasti terbebas dari kanker. <p>
+<input type="radio" name="no2" value="b">b. Sebelum tidur, seseorang harus banyak berolahraga. <p>
+<input type="radio" name="no2" value="c">c. Orang yang terkena kanker pasti tidak pernah berolahraga. <p>
+<input type="radio" name="no2" value="d">d. Berolahraga dapat menghindarkan kita dari berbagai penyakit. <p>
+<input type="radio" name="no2" value="e">e. Kanker merupakan penyakit yang disebabkan oleh kurang tidur. <p>
+</i>
+</ol></tr></td><tr><td>
+<ol start="1" type="a">
+5. Fakta yang diperlukan untuk pendapat-pendapat yang ada pada kutipan itu adalah …. <br><i>
+<input type="radio" name="no2" value="a">a. Cara-cara berolahraga yang baik. <p>
+<input type="radio" name="no2" value="b">b. Proses perkembangbiakan kanker. <p>
+<input type="radio" name="no2" value="c">c. Macam-macam penyakit berbahaya. <p>
+<input type="radio" name="no2" value="d">d. Jumlah penyakit yang biasa menyerang tubuh. <p>
+<input type="radio" name="no2" value="e">e. Pengalaman orang-orang yang biasa olahraga. <p>
+</i>
+</ol></tr></td><tr><td>
+<ol start="1" type="a">
+6. <b>Cermati informasi berikut!</b> 
+Pernyataan (Tesis) <br>
+Saat ini gaya belanja masyarakat sudah banyak yang beralih melalui belanja online. <br>
+Argumentasi <br>
+(1) Metode pembayaran terbatas sehingga belanja aman dan terkendali. <br>
+(2) Hemat waktu dan tenaga sehingga tak perlu lelah mengantre di kasir. <br>
+(3) Varian barang yang dijual hanya barang-barang tertentu dan berkualitas. <br>
+(4) Jenis promo beragam mulai dari potongan harga, poin, hingga voucer belanja. <br>
+(5) Lalu lintas yang padat membuat masyarakat malas keluar rumah untuk belanja. <br>
+Argumentasi yang sesuai dengan pernyataan (tesis) tersebut adalah nomor ....  <br><i>
+<input type="radio" name="no2" value="a">a. (1), (2), dan (3) <p>
+<input type="radio" name="no2" value="b">b. (1), (3), dan (5) <p>
+<input type="radio" name="no2" value="c">c. (2), (3), dan (4) <p>
+<input type="radio" name="no2" value="d">d. (2), (4), dan (5) <p>
+<input type="radio" name="no2" value="e">e. (3), (4), dan (5) <p>
+</i>
+</ol></tr></td><tr><td>
+<ol start="1" type="a">
+7. <b>Cermati teks berikut!</b> <br>
+(1)Saat ini literasi bukan hanya sekadar mampu membaca dan menulis. <br> 
+(2) Melainkan, kemampuan lebih dari sekadar itu. <br>
+(3) Padahal, pemahaman literasi yang mana berarti seorang individu mampu dalam mengolah informasi dan pengetahuan untuk kecakapan hidup. <br>
+(4) Dalam konteks pengajaran, literasi merupakan kemampuan mengakses, memahami, dan menggunakan sesuatu secara cerdas melalui berbagai aktivitas. <br>
+(5) Aktivitas tersebut di antaranya, membaca, berhitung, menyimak, menulis, berbicara dan berbudaya. <br>
+Kalimat yang tidak efektif dari paragraf tersebut adalah kalimat nomor …. <br><i>
+<input type="radio" name="no2" value="a">a. (1) dan (2) <p>
+<input type="radio" name="no2" value="b">b. (1) dan (5) <p>
+<input type="radio" name="no2" value="c">c. (2) dan (3) <p>
+<input type="radio" name="no2" value="d">d. (3) dan (4) <p>
+<input type="radio" name="no2" value="e">e. (4) dan (5) <p>
+</i>
+</ol></tr></td><tr><td>
+<ol start="1" type="a">
+8. <b>Cermati teks berikut!</b> 
+Kebaikan merupakan perbuatan positif yang bersumber dari nilai-nilai agama dan tidak <i>kontra</i> 
+dengan norma dan etika. Keberadaannya sebagai manifestasi dari penghambaan diri seorang 
+makhluk kepada Tuhannya. Setiap kebaikan akan diberikan ganjaran pahala oleh Tuhan, 
+sebaliknya, setiap kejahatan akan <i>mendapatkan</i> sanksi dari-Nya. Akan tetapi, setiap satu 
+kebaikan akan mendapat pahala berlipat ganda, tetapi setiap satu kejahatan hanya dicatat satu 
+dosa saja. Oleh karena itu, kita tidak perlu <i>mengkalkulasikan</i> setiap kebaikan dengan pahala 
+yang akan diterima. Kebaikan bukanlah kebenaran dan kepastian, melainkan ketenangan dan 
+kenyamanan jiwa dan hati. <br>
+Perbaikan kata berimbuhan yang tepat untuk memperbaiki kata yang bercetak miring pada paragraf tersebut adalah .... <br><i>
+<input type="radio" name="no2" value="a">a. kontras, mendapati, mengalkulasikan  <p>
+<input type="radio" name="no2" value="b">b. kontras, mendapatkan, mengkalkulasi <p>
+<input type="radio" name="no2" value="c">c. kontradiktif, mendapati, mengalkulasi <p>
+<input type="radio" name="no2" value="d">d. kontradiksi, mendapatkan, mengkalkulasi <p>
+<input type="radio" name="no2" value="e">e. kontradiktif, mendapatkan, mengalkulasikan <p>
+</i>
+</ol></tr></td><tr><td>
+<ol start="1" type="a">
+9. <b>Cermati keterangan berikut!</b><br> 
+Judul : Problematika Budaya Menyontek di Indonesia <br>
+(1) Berbagai aspek yang melandasi seseorang untuk menyontek.<br> 
+(2) Kebudayaan menyontek semakin marak di Indonesia. <br>
+(3) Pemerolehan hasil atau nilai yang tinggi dan memuaskan<br> 
+(4) Berbagai fakta ditemukan pada setiap tingkat jenjang pendidikan.<br> 
+(5) Simpulan budaya menyontek dengan budaya korupsi di Indonesia <br>
+Urutan kerangka yang sesuai dengan judul adalah nomor …. <br><i>
+<input type="radio" name="no2" value="a">a. (1), (2), (4), (3), (5) <p>
+<input type="radio" name="no2" value="b">b. (2), (4), (1), (3), (5)  <p>
+<input type="radio" name="no2" value="c">c. (3), (2), (3), (1), (5) <p>
+<input type="radio" name="no2" value="d">d. (3), (4), (2), (1), (5) <p>
+<input type="radio" name="no2" value="e">e. (4), (1), (3), (2), (5) <p>
+</i>
+</ol></tr></td><tr><td>
+<ol start="1" type="a">
+10. <b>Cermati teks berikut!</b> <br>
+Predikat kota Yogyakarta bukan hanya sekadar <br>
+(1) kota pelajar dan parawisata, melainkan gudangnya industri kecil, terutama kerajinan. Ribuan Industri kecil kerajinan berkembang di daerah ini, mulai dari skala kecil, misalnya souvenir <br>
+(2) hasil industri rumah tangga hingga skala <br>
+(3) besar yang bertaraf <br>
+(4) internasional. Model-model yang dihasilkan juga sesuai dengan trend <br>
+(5) dunia.<br> 
+Ejaan yang perlu diperbaiki pada kata bernomor yang terdapat pada paragraf tersebut 
+adalah... . <br><i>
+<input type="radio" name="no2" value="a">a. (1) sekadar seharusnya sekedar <p>
+<input type="radio" name="no2" value="b">b. (2) souvenir seharusnya sovenir  <p>
+<input type="radio" name="no2" value="c">c. (3) skala seharusnya sekala  <p>
+<input type="radio" name="no2" value="d">d. (4) bertaraf seharusnya bertarap <p>
+<input type="radio" name="no2" value="e">e. (5) trend seharusnya tren <p>
+</i>
 </ol></tr></td>
-    <tr><td>Soal 1</tr></td>
-    <tr><td>siapa penemu rujak?</tr></td>
-    <tr><td><textarea name="soal1" style="width:300px"></textarea></td></tr>
+    <tr><td>Soal ESSAY</tr></td>
+    <tr><td>1. <b>Apa yang dimaksud dengan teks artikel?</b></tr></td>
+    <tr><td><textarea name="soal1" style="width:500px"></textarea></td></tr>
+	<tr><td></td><td></td></tr>
+	<tr><td>2. <b>Sebutkan dan jelaskan struktur teks artikel?</b></tr></td>
+    <tr><td><textarea name="soal1" style="width:500px"></textarea></td></tr>
+	<tr><td></td><td></td></tr>
+	<tr><td>3. <b>Sebutkan dan jelaskan kaidah kebahasaan teks artikel?</b></tr></td>
+    <tr><td><textarea name="soal1" style="width:500px"></textarea></td></tr>
+	<tr><td></td><td></td></tr>
+	<tr><td>4. <b>Cermati teks berikut!</b><br>
+	Masalah polusi plastik sedang menjadi isu hangat di Indonesia. Sudah menjadi fakta yang 
+	tidak dapat dipungkiri bahwa Indonesia adalah penyumbang terbesar kedua untuk sampah 
+	plastik di laut setelah Tiongkok. Hal itu disebabkan dengan beberapa perkiraan yang 
+	menunjukkan bahwa sampah plastik menyumbang sekitar 10% dari polusi plastik global. 
+	Terlebih lagi, diketahui bahwa empat sungai di Indonesia termasuk di antara 20 sungai yang 
+	paling tercemar di dunia dalam hal sampah plastik. 
+	Plastik tidak dapat terurai dan sayangnya, tidak semua masyarakat Indonesia sadar akan 
+	hal ini. Plastik yang dibuang sembarangan di sungai akan terbawa arus menuju laut. 
+	Selanjutnya, plastik tersebut akan dianggap sebagai makanan bagi burung dan hewan 
+	laut/hewan sekitar laut. Akibatnya, ribuan hewan akan terluka atau mati setiap tahunnya 
+	karena menelan sampah plastik. Seperti pemberitaan akhir-akhir ini, seekor paus sperma 
+	ditemukan mati terdampar di perairan Pulau Kapota, Taman Nasional Wakatobi, Sulawesi 
+	Tenggara. Menurut World Wide Fund for Nature (WWF), paus ini menelan hampir enam 
+	kilogram plastik dan sandal jepit.<br>
+	Fakta pendukung opini yang sesuai isi teks tersebut adalah …
+	</tr></td>
+    <tr><td><textarea name="soal1" style="width:500px"></textarea></td></tr>
+	<tr><td></td><td></td></tr>
+	<tr><td>5. <b>Cermati kutipan berikut!</b><br> 
+	Buncis, Phaseolus vulgaris L, merupakan sayuran sejenis polong-polongan yang dapat 
+	dimanfaatkan buah, biji, dan daunnya. Di Indonesia sayuran ini cukup populer dan banyak 
+	dikonsumsi oleh masyarakat. Sayur ini dikenal sebagai makanan sumber serat, folat, mangan, 
+	serta vitamin C yang baik untuk tubuh. Dengan berbagai kandungan nutrisi tersebut, sayuran 
+	ini mempunyai beberapa manfaat untuk kesehatan. Manfaat sayuran ini, antara lain menjaga 
+	kesehatan jantung, menyehatkan saluran pencernaan, memperkuat sistem kekebalan tubuh, 
+	dan memelihara kesehatan mata. <br>
+	Informasi yang sesuai dengan kutipan tersebut terdapat dalam kalimat....</b></tr></td>
+    <tr><td><textarea name="soal1" style="width:500px"></textarea></td></tr>
 	<tr><td></td><td></td></tr>
 	<tr><td><input type="submit" name="ok" value="Simpan"><input type=reset value=Reset><input type="button" value="Go Back" onclick="history.back(-1)"></td></tr> 
 </form>
@@ -49,6 +215,7 @@ if(isset($_POST['ok'])){
 		$data=$_POST['kelas'];
 		$NO1=$_POST['NO1'];
 		$no2=$_POST['no2'];
+		$no3=$_POST['no3'];
         $soal1=$_POST['soal1'];
 		date_default_timezone_set('Asia/Jakarta');
 		$tanggal=date("h:i:s");
@@ -58,6 +225,7 @@ if(isset($_POST['ok'])){
 		fwrite($buka,"dibuat : ${tanggal} <br> ");
 		fwrite($buka,"NO1 : ${NO1} <br> " );
 		fwrite($buka,"no2 : ${no2} <br> " );
+		fwrite($buka,"no3 : ${no3} <br> " );
         fwrite($buka,"soal1 : ${soal1} <br> ");
 		fwrite($buka,"<hr>");
 		fclose($buka);
